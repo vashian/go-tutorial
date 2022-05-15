e97f44af82c4f480c25491f2d463e18f8ed0b4a4
Mohammadreza <mrvashian@gmail.com>
2022-05-15 19:04:51 +0430

    variable declaration

---
+++ variables/multiple.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		name string 
+		age int
+	)
+
+	name = "Monica"
+	age = 28
+
+
+	var firstName, lastName string = "John", "Doe"
+
+	fmt.Println(name, age)
+	fmt.Println(firstName, lastName)
+}
