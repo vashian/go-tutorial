4acd606a17480188b2644278ca021e43e18121e6
Mohammadreza <mrvashian@gmail.com>
2022-07-12 18:54:04 +0430

    array with slices

--- arrays-slices/sumAllTails.go
+++ arrays-slices/sumAllTails.go
@@ -5,8 +5,13 @@ func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
 
 	for _, numbers := range numbersToSum{
-		tail := numbers[1:]
-		sums = append(sums, Sum(tail))
+
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 
 	return sums
--- arrays-slices/sumAllTails_test.go
+++ arrays-slices/sumAllTails_test.go
@@ -7,14 +7,23 @@ import (
 
 
 func TestSumAllTails(t *testing.T) {
+
+	checkSum := func(t testing.TB, got, want []int) {
+		
+		t.Helper()
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+
 	t.Run("make the sum of some slices", func(t *testing.T) {
 
 		got := SumAllTails([]int{1,2}, []int{0,9})
 		want := []int{2,9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSum(t, got, want)
 	})
 
 	t.Run("Safely sum empty slices", func(t *testing.T) {
@@ -22,9 +31,7 @@ func TestSumAllTails(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3,4,5})
 		want := []int{0, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSum(t, got, want)
 	})
 }
 
