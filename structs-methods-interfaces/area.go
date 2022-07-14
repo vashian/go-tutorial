b3f562face812924827c8c314678f8ba81ce1f42
Mohammadreza <mrvashian@gmail.com>
2022-07-14 19:09:02 +0430

    interfaces in golang

--- structs-methods-interfaces/area.go
+++ structs-methods-interfaces/area.go
@@ -11,6 +11,14 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base float64
+	Height float64
+}
+
+type Shape interface {
+	Area() float64
+}
 
 func (r Rectangle) Area() float64 {
 		return r.Width * r.Height
@@ -19,6 +27,11 @@ func (r Rectangle) Area() float64 {
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
 // func Area(rectangle Rectangle, circle Circle) float64 {
 // 	return rectangle.Height * rectangle.Width
 // }
--- structs-methods-interfaces/area_test.go
+++ structs-methods-interfaces/area_test.go
@@ -2,31 +2,84 @@ package main
 
 import "testing"
 
+
 func TestArea(t *testing.T) {
 
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
+	areaTest := []struct {
+		name string
+		shape Shape
+		hasArea float64
+	} {
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+	}
 
-		got := rectangle.Area()
-		want := 72.0
+	for _, tt := range areaTest {
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
+		t.Run(tt.name, func(t *testing.T) {
 
-	t.Run("circle", func(t *testing.T) {
+			got := tt.shape.Area()
 
-		circle := Circle{10}
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
-	})
+	}
 }
+// func TestArea(t *testing.T) {
+
+// 	checkArea := func (t testing.TB, shape Shape, want float64)  {
+
+// 		t.Helper()
+
+// 		got := shape.Area()
+
+// 		if got != want {
+// 			t.Errorf("got %g want %g", got, want)
+// 		}
+// 	}
+	
+// 	t.Run("rectangle", func(t *testing.T) {
+		
+// 		rectangle := Rectangle{12.0, 6.0}
+
+// 		checkArea(t, rectangle, 72.0)
+// 	});
+	
+// 	t.Run("circle", func(t *testing.T) {
+
+// 		circle := Circle{10.0}
+
+// 		checkArea(t, circle, 314.1592653589793)
+// 	});
+// }
+// func TestArea(t *testing.T) {
+
+// 	t.Run("rectangle", func(t *testing.T) {
+// 		rectangle := Rectangle{12, 6}
+
+// 		got := rectangle.Area()
+// 		want := 72.0
+
+// 		if got != want {
+// 			t.Errorf("got %.2f want %.2f", got, want)
+// 		}
+// 	})
+
+// 	t.Run("circle", func(t *testing.T) {
+
+// 		circle := Circle{10}
+
+// 		got := circle.Area()
+// 		want := 314.1592653589793
+
+// 		if got != want {
+// 			t.Errorf("got %g want %g", got, want)
+// 		}
+// 	})
+// }
 
 // func TestArea(t *testing.T) {
 // 	t.Run("rectangle", func(t *testing.T) {
